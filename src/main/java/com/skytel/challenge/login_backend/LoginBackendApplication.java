0298f562e77eb346fc2dc025f5f4a91aae649fea
roberto <roberto_fernandez@softshop.com.py>
2021-09-06 13:42:14 -0400

    add cors

--- src/main/java/com/skytel/challenge/login_backend/LoginBackendApplication.java
+++ src/main/java/com/skytel/challenge/login_backend/LoginBackendApplication.java
@@ -2,6 +2,9 @@ package com.skytel.challenge.login_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LoginBackendApplication {
@@ -10,4 +13,16 @@ public class LoginBackendApplication {
 		SpringApplication.run(LoginBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/register")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET","POST", "PUT")
+						.maxAge(3600);
+			}
+		};
+	}
 }
